234b7c80b0c9b7f21430a7c6dd8acc78d691b242
Hatim Husain <hatim.husain24@gmail.com>
2026-01-04 20:14:56 +0530

    Question 4 : Code solution for finding the missing number from 1 to n.

---
+++ MissingNumber.java
@@ -0,0 +1,13 @@
+//Find the missing number from 1 to n
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] num = {1,2,3,5,6};
+        int N = num.length + 1;
+        int expectedSum = N * (N+1)/2;
+        int actualSum=0;
+        for (int j : num){
+            actualSum += j;
+        }
+        System.out.println(expectedSum-actualSum);
+    }
+}
