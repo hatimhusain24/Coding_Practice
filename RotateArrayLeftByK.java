f8308e5ae2cdfb76a93eb21e084c2b5b06a36c13
Hatim Husain <hatim.husain24@gmail.com>
2026-01-08 22:50:38 +0530

    Question 7 : Code solution for rotate the array to the left by k

---
+++ RotateArrayLeftByK.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class RotateArrayLeftByK {
+    public static void rotate(int[] arr, int k){
+        int n = arr.length;
+        k = k % n;
+        reverse(arr, 0, n-1);
+        reverse(arr, 0, n-k-1);
+        reverse(arr, n-k, n-1);
+    }
+
+    public static void reverse(int[] arr, int start, int end){
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,8,7,2,6}; // 26587
+        int k = 2;
+        rotate(arr, k);
+        System.out.println(Arrays.toString(arr));
+    }
+}
